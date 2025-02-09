0363540cb19a3d53664572413417c7ff97b55f16
lnog pey <lnogpey@Peylnog-MacBook-Pro.local>
2025-02-09 16:32:23 +0800

    Updated configuration, publications, and other files

---
+++ updata.sh
@@ -0,0 +1,9 @@
+
+# bundle install
+# bundle add webrick
+# bundle exec jekyll server
+
+
+git add -A
+git commit -m "Updated configuration, publications, and other files"
+git push -u origin main --verbose  --force

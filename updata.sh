2ddb550b41cd6694ab9fadc3a021b3dfa656d61e
彭龙 <penglong@Mac.lan>
2025-03-09 22:31:21 +0800

    Updated configuration, publications, and other files

--- updata.sh
+++ updata.sh
@@ -2,14 +2,16 @@
 ###
  # @Date: 2025-02-02 11:48:23
  # @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- # @LastEditTime: 2025-03-01 20:57:20
+ # @LastEditTime: 2025-03-09 22:31:18
  # @FilePath: /peylnog.github.io/updata.sh
 ### 
 bundle install
 bundle add webrick
 bundle exec jekyll server
 
-
+# git config --global http.postBuffer 524288000
+# git config --global http.version HTTP/1.1
 git add -A
 git commit -m "Updated configuration, publications, and other files"
 git push -u origin main --verbose  --force
+# git push --force origin main
